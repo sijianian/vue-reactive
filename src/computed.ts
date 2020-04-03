e5c784b8a2e75efcab0849098afe9be1c269ad83
moke <moke@xiaoman.cn>
2020-04-03 09:46:44 +0800

    :art: feat: init reactive

---
+++ src/computed.ts
@@ -0,0 +1,19 @@
+import Watcher from './watcher'
+
+type computedRef = {
+  value?: any
+}
+
+export default function computed(getter: Function): computedRef {
+  let def = {}
+  const computedWatcher = new Watcher(getter, { computed: true })
+
+  Object.defineProperty(def, 'value', {
+    get() {
+      computedWatcher.depend()
+      return computedWatcher.get()
+    }
+  })
+
+  return def
+}
---
+++ src/reactive.ts
@@ -0,0 +1,34 @@
+import Dep from './dep'
+import { isObject } from './utils'
+
+// 将对象定义成响应式
+export default function reactive<T extends object>(data: T): T {
+  if (isObject(data)) {
+    Object.keys(data).forEach(key => {})
+  }
+  return data
+}
+
+function defineReactive<T extends object, K extends keyof T>(
+  data: T,
+  key,
+  K
+): void {
+  let val = data[key]
+  const dep = new Dep()
+
+  Object.defineProperty(data, key, {
+    get() {
+      dep.depend()
+      return val
+    },
+    set(newVal) {
+      val = newVal
+      dep.notify()
+    }
+  })
+
+  if (isObject(val)) {
+    reactive(val)
+  }
+}
